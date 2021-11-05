677d1fa6ff3edc79c9db0bbfb6e97b24d3ad5e9c
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-11-05 16:29:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# EarnApp earnings watcher
+
+[![axios](https://img.shields.io/github/package-json/dependency-version/LockBlock-dev/earnapp-earnings-watcher/axios)](https://www.npmjs.com/package/axios) [![earnapp-earnings-watcher](https://img.shields.io/github/package-json/dependency-version/LockBlock-dev/earnapp-earnings-watcher/earnapp-earnings-watcher)](https://www.npmjs.com/package/earnapp-earnings-watcher) [![simple-discord-webhooks](https://img.shields.io/github/package-json/dependency-version/LockBlock-dev/earnapp-earnings-watcher/simple-discord-webhooks)](https://www.npmjs.com/package/simple-discord-webhooks)
+
+[![GitHub stars](https://img.shields.io/github/stars/LockBlock-dev/earnapp-earnings-watcher.svg)](https://github.com/LockBlock-dev/earnapp-earnings-watcher/stargazers)
+
+Send a report of your hourly earning in a channel via a Discord webhook
+
+![](preview.jpg)
+
+## Installation
+
+-   Install [NodeJS](https://nodejs.org).
+-   Download or clone the project.
+-   Go to the `earnapp-earnings-watcher` folder and run `npm install`.
+-   In the [index.js](./index.js), you need to edit 3 things:
+
+```js
+const postman = new Webhook("WEBHOOK URL");
+//Add your own Discord WebHook URL
+```
+
+```js
+client.login({
+    authMethod: "google",
+    oauthRefreshToken: "COOKIE", //see https://github.com/LockBlock-dev/earnapp.js#how-to-login-with-cookies
+});
+//Add your own EarnApp cookie
+```
+
+```js
+const getName = (uuid) => {
+    switch (uuid) {
+        case "sdk-XXX-XXX":
+            return "cool pc";
+        case "sdk-XXX-XXX":
+            return "my cool VPS";
+        default:
+            return uuid;
+    }
+};
+//Add a case "uuid" + return "device name" for each device you want to name
+```
+
+-   Run `node index.js` or `npm start`.
+
+## Credits
+
+[EarnApp](https://earnapp.com)
+
+## Copyright
+
+See the [license](/LICENSE)
---
+++ index.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const { Client } = require("earnapp.js");
+const { Webhook } = require("simple-discord-webhooks");
+
+const client = new Client();
+const postman = new Webhook("WEBHOOK URL");
+
+client.login({
+    authMethod: "google",
+    oauthRefreshToken: "COOKIE", //see https://github.com/LockBlock-dev/earnapp.js#how-to-login-with-cookies
+});
+
+let first = true;
+
+const delay = async (ms) => {
+    return await new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const getName = (uuid) => {
+    switch (uuid) {
+        case "sdk-XXX-XXX":
+            return "cool pc";
+        default:
+            return uuid;
+    }
+};
+
+const bytesToSize = (bytes) => {
+    const sizes = ["B", "KB", "MB", "GB", "TB"];
+    if (bytes === 0) return "0 B";
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+    if (i === 0) return `${bytes} ${sizes[i]}`;
+    return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+};
+
+const getDevices = async () => {
+    const devices = await client.devices();
+    fs.writeFileSync("./old.json", JSON.stringify(devices, null, 1), "utf8");
+    return devices;
+};
+
+const getEarnings = async () => {
+    const earnings = [];
+    const old = JSON.parse(fs.readFileSync("./old.json", "utf8"));
+    const data = await getDevices();
+    data.forEach((device, i) => {
+        if (device.total_bw > 0.0) {
+            earnings.push({
+                name: getName(device.uuid),
+                usage: `+ ${bytesToSize(device.bw - old[i].bw)}`,
+                amount: `+ ${(device.earned - old[i].earned).toFixed(2)}$`,
+            });
+        }
+    });
+
+    return earnings;
+};
+
+const run = async () => {
+    while (true) {
+        if (first) {
+            await getDevices();
+        }
+        const earnings = await getEarnings();
+        let fields = [];
+        earnings.forEach((e) => {
+            fields.push({
+                name: e.name,
+                value: `${e.usage} | ${e.amount}`,
+            });
+        });
+        postman.send("", [
+            {
+                title: "EarnApp gains report",
+                fields: fields,
+            },
+        ]);
+        await delay(1000 * 60 * 60);
+    }
+};
+
+run();
